ab2edf7e1d80cb906d58d35dbb6030e3ed96bc7d
加藤北斗 <hokuto15@gmail.com>
2019-05-09 17:56:22 +0900

    環境構築

---
+++ gulp/config.js
@@ -0,0 +1,44 @@
+const paths = {
+	img_src: './src/img/',
+	img_dest: './dest/img/',
+	img_build: './docs/img/',
+
+	js_src: './src/js/',
+	js_dest: './dest/js/',
+	js_build: './docs/js/',
+
+	stylus_src: './src/stylus/app.styl',
+	stylus_dest: './dest/css/',
+	stylus_component_dest: './src/stylus/object/component/',
+	stylus_build: './docs/css/',
+
+	pug_src: './src/pug/**/*.pug',
+	pug_include: './src/pug/include/',
+	pug_exclude: '!./src/pug/**/_*.pug',
+
+	yaml_src: './src/yaml/**/*.y{,a}ml',
+	yaml_dest: './dest/json/',
+
+	sprite_src: './src/sprite/',
+	sprite_dest: './src/img/',
+
+	iconfont_src: './src/iconfont/*.svg',
+	iconfont_stylus_dest: './src/stylus/object/component/',
+	iconfont_dest: './dest/font/',
+	iconfont_build: './dest/font/',
+	
+	webfont_src: './src/webfont/*.*',
+	font_dest: './dest/webfont/',
+	font_build: './docs/font/',
+	
+	favicon_src: './src/favicon/',
+	favicon_dest: './dest/favicon/',
+	
+	template_src: './src/template/',
+	template_dest: './dest/template/',
+
+	dest: './dest/',
+	build: './docs/',
+};
+
+export default paths;
---
+++ gulp/tasks/sprite.js
@@ -0,0 +1,92 @@
+import gulp from 'gulp';
+import spritesmith from 'gulp.spritesmith';
+import buffer from 'vinyl-buffer';
+import merge from 'merge-stream';
+import imagemin from 'gulp-imagemin';
+import pngquant from 'imagemin-pngquant';
+import svgSprite from 'gulp-svg-sprite'
+import svgmin from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import inject from 'gulp-inject';
+import html2jade from 'gulp-html2jade'
+import rename from 'gulp-rename';
+import paths from '../config';
+
+// png
+gulp.task('spritePng', () => {
+	const spriteData = gulp.src(`${paths.sprite_src}png/*.png`)
+	.pipe(spritesmith({
+		imgName: '../img/sprite.png',
+		cssName: 'sprite.styl',
+		padding: 5
+	}));
+
+	const imgStream = spriteData.img
+	.pipe(buffer())
+	.pipe(imagemin(
+		[pngquant()]
+	))
+	.pipe(imagemin())
+	.pipe(gulp.dest(`${paths.sprite_dest}`));
+
+	const cssStream = spriteData.css
+	.pipe(gulp.dest(`${paths.stylus_component_dest}`));
+	return merge(imgStream, cssStream);
+});
+
+
+//svg
+gulp.task('spriteSvg', () => {
+	return gulp.src(`${paths.sprite_src}svg/*.svg`)
+	.pipe(svgSprite({
+		mode: {
+			symbol: {
+				// スプライト画像を置くディレクトリ名
+				dest: './',
+				// スプライト画像のファイル名
+				sprite: 'sprite.svg',
+				// スプライト画像のプレビュー用HTML
+				example: {
+					dest: '../../dest/template/svg/sprite.html',
+				}
+			},
+		},
+		// mode
+		shape: {
+			transform: [
+				{
+					svgo: {
+						plugins: [
+							{
+								// plugin記述
+							},
+						]
+					}
+				}
+			]
+		}
+	}))
+	// 書き出し先
+	.pipe(gulp.dest(`${paths.img_src}`));
+});
+
+// svg inline
+gulp.task('svgSprteInline', () => {
+	const svgs = gulp
+	.src(`${paths.sprite_src}svg-inline/*.svg`)
+	.pipe(svgmin())
+	.pipe(svgstore({inlineSvg: true}));
+
+	function fileContents(filePath, file) {
+		return file.contents.toString();
+	}
+
+	return gulp
+	.src(`${paths.template_src}svgsprite-inline/svgsprite-inline.html`)
+	.pipe(inject(svgs, {transform: fileContents}))
+	.pipe(html2jade())
+	.pipe(rename({
+		extname: '.pug'
+	}))
+	.pipe(gulp.dest(`${paths.template_dest}svgsprite-inline/`));
+});

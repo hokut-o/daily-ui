8dc7d2714ad2864417217c82afdbb9c756f45de4
加藤北斗 <hokuto15@gmail.com>
2019-05-24 14:04:44 +0900

    初期化とモジュールのアップデート

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,1 @@
 import $ from "jquery"
-import slick from "slick-carousel"
-
-$('[data-target-carousel]').slick({
-	centerMode: true,
-	slidesToShow: 1,
-	dots: false,
-	arrows: false,
-	variableWidth:true
-});

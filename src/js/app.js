38d6394ee6cc5ff28ad9080e72bd510c9b4a5749
加藤北斗 <hokuto15@gmail.com>
2019-05-26 13:11:01 +0900

    スタイリング、アコーディオン、build

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
-import $ from "jquery"
+import $ from "jquery";
 
+const trigger = {
+	accordion: $("[data-trigger-accordion]")
+};
 
+const toggleAccordion = () => {
+	trigger.accordion.on("click", e => {
+		$(e.currentTarget)
+		.toggleClass("is-active")
+		.next()
+		.slideToggle("fast", "swing");
+	});
+};
+
+$(() => {
+	toggleAccordion();
+});

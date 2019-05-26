af597e30c5475813c84f028a75c87f3c9904f1b9
加藤北斗 <hokuto15@gmail.com>
2019-05-26 15:56:48 +0900

    Webpack4圧縮時にJavaScriptのコメントを全て削除する

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,9 +1,26 @@
+import TerserPlugin from 'terser-webpack-plugin'
+
 module.exports = {
 	mode: 'production',
 	entry: './src/js/app.js',
 	output: {
 		path: `${__dirname}/dest/js`,
 		filename: 'app.js'
+	},
+	plugins: [],
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					compress: {
+						drop_console: true,
+					},
+					output: {
+						comments: false,
+					},
+				},
+			}),
+		],
 	},
 	module: {
 		rules: [

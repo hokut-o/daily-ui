83dd383074c7dfb89df58b33427184a1595c0cb6
加藤北斗 <hokuto15@gmail.com>
2019-06-24 16:36:02 +0900

    range取得、表示、build

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-import $ from "jquery"
+import $ from "jquery";
+
+$("[data-trigger-range]").on("input", e => {
+	let rangeVal = $(e.currentTarget).val();
+	changeRangeStatus(rangeVal);
+});
+
+const changeRangeStatus = val => {
+	$("[data-target-range]").text(val);
+};
